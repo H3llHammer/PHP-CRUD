ead3ae195ddaed0b147abac86bcff3f11d4566c3
Alberto T. Fuentes Ramirez <albertofuentes505@outlook.com>
2021-08-05 19:30:14 -0500

    Basic form to add a new task

--- index.php
+++ index.php
@@ -2,6 +2,29 @@
 <?php include("includes/header.php") ?>
 <?php include("includes/navigation.php") ?>
 
-    <h1>hola mundo</h1>
+<div class="container p-4">
+
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card card-body">
+                <form action="save.php" method="POST">
+                    <div class="form-group">
+                        <input type="text" name="title" class="form-control" placeholder="Task title" autofocus>
+                    </div>
+                    
+                    <div class="form-group">
+                        <textarea name="description" rows="2" class="form-control" placeholder="Task description"></textarea>
+                    </div>
+                    <input type="submit" class="btn btn-success btn-block" name="save_task" value="Save">
+                </form>
+            </div>
+        </div>
+
+        <div class="col-md-8">
+
+        </div>
+    </div>
+
+</div>
 
 <?php include("includes/footer.php") ?>
